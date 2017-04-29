cb6ef17c29506b35f626c5e3de60c1dd29e4a5d3
Leo Reiter <leo.reiter@nimbix.net>
2017-04-29 17:24:15 -0500

    fetch appropriate raw link for notebook-site file

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -11,7 +11,7 @@ pip3 install --upgrade notebook
 
 cd /etc/nginx/sites-enabled
 curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/default
-curl -H 'Cache-Control: no-cache' -O https://github.com/nimbix/notebook-common/blob/master/conf/notebook-site
+curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/notebook-site
 cd /etc/nginx/conf.d
 curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/httpredirect.conf
 cd /usr/local/bin
