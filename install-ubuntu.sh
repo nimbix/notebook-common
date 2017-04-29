b2f3a97fec592c9dff60f474edb7c810b69adee7
Leo Reiter <leo.reiter@nimbix.net>
2017-04-29 16:56:14 -0500

    initial checkin

---
+++ install-ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+set -e
+
+apt-get update
+apt-get -y install python-pip nginx sudo
+pip install --upgrade notebook
+
+cd /etc/nginx/sites-enabled
+curl -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/default
+curl -O https://github.com/nimbix/notebook-common/blob/master/conf/notebook-site
+cd /etc/nginx/conf.d
+curl -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/httpredirect.conf
+cd /usr/local/bin
+curl -O https://raw.githubusercontent.com/nimbix/notebook-common/master/nimbix_notebook
+chmod 555 /usr/local/bin/nimbix_notebook
+
+
