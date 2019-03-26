ef4adf85310d83761a75541a6fdfc37f715dd324
Kenneth Hill <ken.hill@nimbix.net>
2019-03-26 15:49:04 -0400

    Add support for CentOS 7
    Signed-off-by: Kenneth Hill <ken.hill@nimbix.net>

--- install-centos.sh
+++ install-centos.sh
@@ -32,12 +32,31 @@
 set -x
 set -e
 
+while getopts "b:p" opt; do
+    case ${opt} in
+        b)
+            BRANCH="${OPTARG}"
+            ;;
+        p)
+            PYTHON="3"
+            ;;
+        *)
+            echo "usage: $0 [-b <branch>] [-p <notebook-port>]" >&2
+            echo "  use -b to specify notebook common branch" >&2
+            echo "  use -p to specify to use python3" >&2
+            exit 1
+            ;;
+    esac
+done
+
 #yum update -y
 yum groupinstall -y "Development Tools"
-yum install -y nginx sudo curl python-devel zeromq-devel
+yum install -y wget curl python-devel zeromq-devel
+yum install -y epel-release
+yum makecache
 
 # Python 3, must choose python 3.4 or 3.6, defaulting here to 3.6
-if [[ "$1" = "3" ]]; then
+if [[ "${PYTHON}" = "3" ]]; then
     yum install -y python36-pip
     python36 -m pip install --upgrade pip setuptools
     pip3 install --upgrade packaging appdirs jupyter
@@ -48,17 +67,25 @@ else
 fi
 yum clean all
 
-mkdir /etc/nginx/sites-enabled && cd /etc/nginx/sites-enabled
-curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/notebook-site
-cd /etc/nginx
-curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/testing/conf/nginx.conf
+# Install redir
+REDIR_VERSION="v2.2.1"
+mkdir /tmp/build-redir
+cd /tmp/build-redir
+wget https://github.com/troglobit/redir/archive/${REDIR_VERSION}.tar.gz
+tar -xf *.tar.gz
+cd redir*
+make all
+cp redir /usr/bin
+cd
+rm -rf /tmp/build-redir
+yum groupremove -y "Development Tools"
+
+[[ -z ${BRANCH} ]] && BRANCH="master"
+
 cd /usr/local/bin
-curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/master/nimbix_notebook
+curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/${BRANCH}/nimbix_notebook
 chmod 555 /usr/local/bin/nimbix_notebook
 
 mkdir -p /etc/NAE
-echo "https://%PUBLICADDR%/" >/etc/NAE/url.txt
+echo "https://%PUBLICADDR%/?token=%RANDOM64%" >/etc/NAE/url.txt
 
-# for JARVICE emulation
-mkdir -p /etc/JARVICE && cd /etc/JARVICE
-curl -H 'Cache-Control: no-cache' -O https://raw.githubusercontent.com/nimbix/notebook-common/master/conf/htpasswd
